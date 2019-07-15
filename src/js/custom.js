263c4af553e1d867f297c68a9dc87f23b1a51393
1905-ActivityWeb <smileful3@gmail.com>
2019-07-15 10:53:11 +0800

    动画完成

--- src/js/custom.js
+++ src/js/custom.js
@@ -24,6 +24,73 @@ $(document).ready(function () {
             $('.PageBox').removeClass('animation');
             $('.PageBox').eq(index).addClass('animation');
 
+            // Wrap every letter in a span - PBi-02 h2
+            $('.ml2, .ml3, .ml4, .ml6').each(function(){
+                $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            });
+            // $('.ml3').each(function(){
+            //     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            // });
+            // $('.ml4').each(function(){
+            //     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            // });
+            // $('.ml6').each(function(){
+            //     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            // });
+
+            anime.timeline({loop: false})
+            .add({
+                targets: '.ml2 .letter',
+                scale: [4,1],
+                opacity: [0,1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 950,
+                delay: function(el, i) {
+                return 100*i;
+                }
+            });
+
+            anime.timeline({loop: false})
+            .add({
+                targets: '.ml3 .letter',
+                scale: [4,1],
+                opacity: [0,1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 950,
+                delay: function(el, i) {
+                return 100*i;
+                }
+            });
+
+            anime.timeline({loop: false})
+            .add({
+                targets: '.ml4 .letter',
+                scale: [4,1],
+                opacity: [0,1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 950,
+                delay: function(el, i) {
+                return 100*i;
+                }
+            });
+
+            anime.timeline({loop: false})
+            .add({
+                targets: '.ml6 .letter',
+                scale: [4,1],
+                opacity: [0,1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 950,
+                delay: function(el, i) {
+                return 100*i;
+                }
+            });
+            
+
             canMove = false;
 
             // 每次捲動畫面都要一定間隔
@@ -31,6 +98,9 @@ $(document).ready(function () {
                 canMove = true;
             }, 500);
         });
+
+        
+
     }
 
     else {
